2c31896ae3c1b7375090c481e74628a2a0c8df35
Eben Varghese Paul <810192@lulumea.com>
2021-05-16 18:31:07 +0400

    Added BinarySearch,Insertion Sort

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,18 @@ namespace SortProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Different Algoriths and Searches!");
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Merge Sort!");
             Console.Write("");
             List<int> unsorted = new List<int>();
             List<int> sorted;
-
             Random random = new Random();
             string tabs = "=";
-            Console.WriteLine("Original array elements:");
 
+            #region RandomUnSortedElements
+            Console.WriteLine("Original array elements:");
             for (int i = 0; i < 10; i++)
             {
                 int randomno = random.Next(0, 100);
@@ -30,83 +32,87 @@ namespace SortProgram
                 tabs = new String('=', element);
                 Console.Write(tabs + " " + element + "\n");
             }
+            int[] array = new int[unsorted.Count];
+            for (int i = 0; i < unsorted.Count; i++)
+            {
+                array[i] = Convert.ToInt32(unsorted[i]);
+            }
             Console.WriteLine();
+            #endregion
 
             Console.Write("\n");
-            sorted = MergeSort(unsorted);
-            Console.WriteLine("Sorted array elements: ");
 
-            foreach (int x in sorted)
+
+            Console.WriteLine("Select the Operation");
+            Console.WriteLine("Enter 1 for Merge Sort");
+            Console.WriteLine("Enter 2 for Insertion Sort");
+            Console.WriteLine("Enter 3 for Bianry Search");
+            Console.WriteLine("Enter Your Prefered Operation");
+            var _Input = Console.ReadLine();
+            int intInput = 0;
+            if (! string.IsNullOrEmpty(_Input))
             {
-                tabs = new String('=', x);
-                Console.Write(tabs + " " + x + "\n");
-         
+                intInput = Information.IsNumeric(_Input) ? Convert.ToInt16(_Input) : 0;
             }
-            Console.Write("\n");
+            switch (intInput)
+            {
+                case 1:
+                    Console.WriteLine("Selected Merge Sort");
+                    #region MergeSortedElements
+                    MergeSortAlgo mergeSortAlgo = new MergeSortAlgo();
+                    sorted = mergeSortAlgo.MergeSort(unsorted);
+                    Console.WriteLine("Merge Sorted array elements: ");
+                    foreach (int x in sorted)
+                    {
+                        tabs = new String('=', x);
+                        Console.Write(tabs + " " + x + "\n");
+                    }
+                    #endregion
+                    Console.Write("\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Selected Insertion Sort");
 
+                    #region InsertionSortedElements
+                    InsertionSortAlgo insertionSortAlgo = new InsertionSortAlgo();
+                    Console.WriteLine("Insertion Sorted array elements: ");
+                   
+                    foreach (int x in insertionSortAlgo.InsertionSort(array))
+                    {
+                        tabs = new String('=', x);
+                        Console.Write(tabs + " " + x + "\n");
+                    }
+                    #endregion
+                    break;
+
+                case 3:
+                    Console.WriteLine("Selected Binary Search");
+                    #region BinarySearch
+                    Console.WriteLine("Enter the Element to be Searched Search");
+                    BinarySearchAlgo binarySearchAlgo  = new BinarySearchAlgo();
+                    var SearchNode = Console.ReadLine();
+                    Console.WriteLine("You have Entered : " +SearchNode);
+                    int intSearchNode = 0;
+                    if (!string.IsNullOrEmpty(SearchNode))
+                    {
+                        intSearchNode = Information.IsNumeric(SearchNode) ? Convert.ToInt16(SearchNode) : 0;
+                    }
+                    object obj= binarySearchAlgo.BinarySearchIterative(array, intSearchNode);
+                    if (obj != null) Console.WriteLine("The Node of Seached Item "+ intSearchNode + "  is  : " + obj);
+                    #endregion
+                    break;
+
+                default:
+                    Console.WriteLine("Selected Wrong Option");
+                    break;
+            }
+
+            Console.Write("\n");
             Console.ReadKey();
-
-
         }
 
 
-        private static List<int> MergeSort(List<int> unsorted)
-        {
-            if (unsorted.Count <= 1)
-                return unsorted;
-
-            List<int> left = new List<int>();
-            List<int> right = new List<int>();
-
-            int middle = unsorted.Count / 2;
-            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
-            {
-                left.Add(unsorted[i]);
-            }
-            for (int i = middle; i < unsorted.Count; i++)
-            {
-                right.Add(unsorted[i]);
-            }
-
-            left = MergeSort(left);
-            right = MergeSort(right);
-            return Merge(left, right);
-        }
-
-
-        private static List<int> Merge(List<int> left, List<int> right)
-        {
-            List<int> result = new List<int>();
-
-            while (left.Count > 0 || right.Count > 0)
-            {
-                if (left.Count > 0 && right.Count > 0)
-                {
-                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
-                    {
-                        result.Add(left.First());
-                        left.Remove(left.First());      //Rest of the list minus the first element
-                    }
-                    else
-                    {
-                        result.Add(right.First());
-                        right.Remove(right.First());
-                    }
-                }
-                else if (left.Count > 0)
-                {
-                    result.Add(left.First());
-                    left.Remove(left.First());
-                }
-                else if (right.Count > 0)
-                {
-                    result.Add(right.First());
-
-                    right.Remove(right.First());
-                }
-            }
-            return result;
-        }
+       
     }
 
 }
